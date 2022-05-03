032b216d193e7f8aee892c458cef36eff87e6da5
Philip Levy <philipdlevy@gmail.com>
2022-05-03 14:20:51 -0600

    commit tree 4d836a325a68ad5b0386a0a291c49948169a5c2c

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import SignUp from "./SignUp";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function SignUp() {
+    return (
+        <div>
+            <h1>SignUp</h1>
+            <form>
+                <div>
+                <input type="text" name="username" placeholder="Username" />
+                </div>
+                <div>
+                <input type="password" name="password" placeholder="Password" />
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+};
+
+export default SignUp;
